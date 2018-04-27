9dec4fe098f729f36574a6f79ba400367b4d2f9e
renzheng <renz@koal.com>
2018-04-28 05:33:33 +0800

    add show bytes functions

---
+++ practice-problem/chapter2/pp2_5.c
@@ -0,0 +1,12 @@
+#include "show_byte.h"
+
+int main()
+{
+  int val = 0x87654321;
+  byte_pointer valp = (byte_pointer) &val;
+  show_bytes(valp, 1);
+  show_bytes(valp, 2);
+  show_bytes(valp, 3);
+
+  return 0;
+}
